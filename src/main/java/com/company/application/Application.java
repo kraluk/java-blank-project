f1fa3725baab1acd074a7752cdef6628fe1f8cdd
Łukasz Krauzowicz <kraluk@gmail.com>
2020-03-16 11:14:47 +0100

    Initial project commit

---
+++ src/main/java/com/company/application/Application.java
@@ -0,0 +1,10 @@
+package com.company.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.util.Objects.requireNonNull;
+
+public final class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+}
---
+++ src/test/java/com/company/application/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.company.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest {
+
+    @Test
+    void test() {
+        assertThat(true)
+            .isTrue();
+    }
+}
