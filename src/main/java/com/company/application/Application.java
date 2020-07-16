cb11cc1a6ce5b7fe242593de923b8aa9b32df16c
Łukasz Krauzowicz <kraluk@gmail.com>
2020-07-16 11:40:49 +0200

    Reformat the code base

--- src/main/java/com/company/application/Application.java
+++ src/main/java/com/company/application/Application.java
@@ -3,8 +3,6 @@ package com.company.application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static java.util.Objects.requireNonNull;
-
 public final class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 }
